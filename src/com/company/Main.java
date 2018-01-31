de2cb9753cb9cc75fcd5c8875652aa798cd70cea
Tlenc <tlenciauskas@gmail.com>
2018-01-31 15:55:39 +0200

    Code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,28 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
+    private String[] mas= new String[5];
+
     public static void main(String[] args) {
-	// write your code here
+        Main objektas = new Main();
+        objektas.Masyvas();
+        objektas.Rasymas();
+    }
+
+    public void Masyvas(){
+        Scanner sc = new Scanner(System.in);
+        mas = new String[5];
+        for (int i=0; i<mas.length; i++){
+            System.out.println("Įveskite žodį: ");
+            mas[i]=sc.nextLine();
+        }
+    }
+
+    public void Rasymas(){
+        for (int i=0; i<mas.length; i++){
+            System.out.println("Masyvo " + i + " zodis yra "+mas[i]);
+        }
     }
 }
